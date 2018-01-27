c580e5975c24f3fd003ce3902f00133797ccfbf0
luis.chavez <luchor13@gmail.com>
2018-01-27 00:09:38 -0500

    POM actualizado y formateado

--- src/main/java/com/roche/beneficios/BeneficiosApplication.java
+++ src/main/java/com/roche/beneficios/BeneficiosApplication.java
@@ -1,12 +1,25 @@
 package com.roche.beneficios;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class BeneficiosApplication {
+public class BeneficiosApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
-		SpringApplication.run(BeneficiosApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(BeneficiosApplication.class, args);
+    }
+    
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BeneficiosApplication.class);
 	}
 }
